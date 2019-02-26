6de453626eb80b7879fc18a2c714fa16b928c6f2
MadboyJames <Jkc36@pitt.edu>
2019-02-26 12:19:12 +0000

    prepared create enviorment

--- eco_lab_v02/create_environment.m
+++ eco_lab_v02/create_environment.m
@@ -16,5 +16,5 @@ global ENV_DATA
 ENV_DATA.shape='square';
 ENV_DATA.units='kilometres';
 ENV_DATA.bm_size=size;
-ENV_DATA.food=floor(50*ones(size,size));        %distribute food in km x km squares 
-ENV_DATA.food(round(0.6*size):round(0.8*size),round(0.6*size):round(0.8*size))=0;   %generate patch where there is no food
+ENV_DATA.food=floor(50*ones(size,size));        %distribute food in km x km squares (Change to be Max Density which will be 3)
+ENV_DATA.food(round(0.6*size):round(0.8*size),round(0.6*size):round(0.8*size))=0;   %generate patch where there is no food (change to be current density, which will be 0)
