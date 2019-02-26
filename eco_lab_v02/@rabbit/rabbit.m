da90e751e8bba18fc32bd30e66234f112aabab09
MadboyJames <Jkc36@pitt.edu>
2019-02-26 12:16:13 +0000

    Prepared Rabbit (Will be "Herring")

--- eco_lab_v02/@rabbit/rabbit.m
+++ eco_lab_v02/@rabbit/rabbit.m
@@ -1,30 +1,30 @@
 classdef rabbit   %declares rabbit object
     properties    %define rabbit properties (parameters) 
-        age; 
-        food;
+        age; %(DELETE)
+        food;%(DELETE)
         pos;
-        speed;
-        last_breed;
+        speed; %Perception 
+        last_breed; %(DELETE)
     end
     methods                         %note that this class definition mfile contains only the constructor method!
                                     %all additional member functions associated with this class are included as separate mfiles in the @rabbit folder. 
         function r=rabbit(varargin) %constructor method for rabbit - assigns values to rabbit properties
                 %r=rabbit(age,food,pos....)
                 %
-                %age of agent (usually 0)
-                %food - amount of food that rabbit has eaten
-                %pos - vector containg x,y, co-ords 
+                %age of agent (usually 0) (DELETE)
+                %food - amount of food that rabbit has eaten (DELETE)
+                %pos - vector containg x,y, co-ords  
 
                 %Modified by Martin Bayley on 29/01/13
 
 
                 switch nargin           %Use switch statement with nargin,varargin contructs to overload constructor methods
                     case 0				%create default object
-                       r.age=[];			
-                       r.food=[];
+                       r.age=[];		%(DELETE)	
+                       r.food=[]; %(DELETE)
                        r.pos=[];
                        r.speed=[];
-                       r.last_breed=[];
+                       r.last_breed=[]; %(DELETE)
                     case 1              %input is already a rabbit, so just return!
                        if (isa(varargin{1},'rabbit'))		
                             r=varargin{1};
@@ -33,7 +33,7 @@ classdef rabbit   %declares rabbit object
                             
                        end
                     case 5               %create a new rabbit (currently the only constructor method used)
-                       r.age=varargin{1};               %age of rabbit object in number of iterations
+                       r.age=varargin{1};               %age of rabbit object in number of iterations 
                        r.food=varargin{2};              %current food content (arbitrary units)
                        r.pos=varargin{3};               %current position in Cartesian co-ords [x y]
                        r.speed=varargin{4};             %number of kilometres rabbit can migrate in 1 day
