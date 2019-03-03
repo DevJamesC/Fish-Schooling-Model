62fa3fae2bf59b05d7001edd9cb41b9ea321d4b7
Hasan Asim <hasan.asim134@gmail.com>
2019-03-03 19:24:40 +0000

    create_fish function

--- fish_schooling/create_fish.m
+++ fish_schooling/create_fish.m
@@ -1,32 +1,26 @@
 function [agents] = create_fish(nf)
 %CREATE_FISH Summary of this function goes here
 %   Detailed explanation goes here
+% creates the objects representing each fish
 
-global ENVIRONMENT
+global ENVIRONMENT PARAM
 
 s=ENVIRONMENT.size;
-%positions= round((s-1)*rand(nf,2)+1);  
- 
+
     for f=1:nf
         pos = round((s-1)*rand(1, 2)+1);
+        % if the position is full, keep looping until a position is found
+        % that has space available 
         while ~(ENVIRONMENT.space(pos(1, 1), pos(1,2)) < MAX_DENSITY)
             pos = round((s-1)*rand(1, 2)+1);
             %will be a 1x2 matrix like [1,2]
         end
-        agents{f} = fish(pos);
+        agents{f} = fish(pos,PARAM.MAX_DENSITY);
         %perhaps use populate function to populate environment using code
         %below so like
         %populate(pos);
         ENVIRONMENT.space(pos(1, 1), pos(1, 2)) = ENVIRONMENT.space(pos(1, 1), pos(1, 2)) + 1;
         %like array value calling another array value
     end
-
-    %for f=1:nf
-    %    agents{f}=fish(positions(f, :));
-    %end
-    
-% for f=1:nf
-%      agents{f} = fish(round((s-1)*rand(1, 2)+1));
-% end
 end
 
