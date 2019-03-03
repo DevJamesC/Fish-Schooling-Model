3a4adeae28859d6e91a11964291b662901d9690d
Hasan Asim <hasan.asim134@gmail.com>
2019-03-03 16:24:47 +0000

    implemented get function

--- fish_schooling/@fish/get.m
+++ fish_schooling/@fish/get.m
@@ -1,4 +1,14 @@
-function position = get(f, position)
-position = f.position;
+function val=get(c,prop_name)
+
+%standard function to allow extraction of memory parameters from fish object
+
+switch prop_name
+   
+case 'position'
+   val=c.position;
+case 'perception'
+   val=c.perception; 
+otherwise 
+   error('invalid field name')
 end
 
