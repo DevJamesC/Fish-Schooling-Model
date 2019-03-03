2f7e0c7c7b62a85ca9136279f423898c9beb6095
Hasan Asim <hasan.asim134@gmail.com>
2019-03-03 15:56:56 +0000

    implemented fish class and constructor

--- fish_schooling/@fish/fish.m
+++ fish_schooling/@fish/fish.m
@@ -1,22 +1,27 @@
-classdef fish
-    %not sure if pereception should be a parameter however if it is then
-    %call construction with fish(pos, PARAM.Perception), making perception,
-    %but all the parameter are hard coded into the programme and not given
-    %by user input.
-    properties
+classdef fish %declares fish object
+    properties %define fish properties (parameters)
         position;
         perception;
     end
-    
     methods
-        function f = fish(varargin)
-            switch nargin
-                case 0
+        function f = fish(varargin) %constructor method for fish - assigns values to fish properties 
+            % f = rabbit(position,perception)
+            % position - vector containing x,y coordinates
+            % perception - integer value representing how many squares fish
+            % can migrate
+            switch nargin 
+                case 0 %create default object 
                     f.position = [];
-                    f.perception = P_LENGTH;
-                case 1 
+                    f.perception = [];
+                case 1 % input is already a fish so return
+                    if(isa(varargin{1},'fish'))
+                        f=varargin{1};
+                    else
+                        error('Input argument is not a fish')
+                    end
+                case 2 %create a new fish 
                     f.position = varargin{1};
-                    f.perception = P_LENGTH;
+                    f.perception = varargin{2};
                 otherwise
                     error('Invalid no. of input arguments for fish');
             end
