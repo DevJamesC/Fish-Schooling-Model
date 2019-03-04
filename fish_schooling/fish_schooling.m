7aa172c5c417894783fd5cee728a729f7aa91a67
Hasan Asim <hasan.asim134@gmail.com>
2019-03-04 12:11:06 +0000

    structuring fish_schooling.m

--- fish_schooling/fish_schooling.m
+++ fish_schooling/fish_schooling.m
@@ -1,11 +1,30 @@
-function fish_schooling(size, nf, perception_length, nsteps)
+function fish_schooling(size, nf, nsteps)
 %FISH_SCHOOLING Summary of this function goes here
 %   Detailed explanation goes here
+% FISH_SCHOOLING agent-based herring schooling model
+% COM3001
 
-global ENVIRONMENT P_LENGTH N_IT MAX_DENSITY
+%ecolab(size,nf,perception_length,nsteps)
+%size = size of model environment in m
+%nf - number of fish agents
+%nsteps - number of iterations required
 
-P_LENGTH = perception_length;
+global ENVIRONMENT PARAM N_IT
+% definition of global variables
+% N_IT - current iteration number 
+% PARAM - contains the max density 
+% ENVIRONMENT - data structure representing the environment
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%MODEL INITIALISATION
+initialise_params;                      %sets the parameters for this simulation
+create_environment(size);           %creates environment data structure, given an environment size
+%random_selection(1);                %randomises random number sequence (NOT agent order). If input=0, then simulation should be identical to previous for same initial values
+[agent]=create_fish(nr,nf);       %creates nf fish agents and places them in a cell array called 'agents'
+%initialise_results(nr,nf,nsteps);   %initilaises structure for storing results
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%MODEL EXECUTION
 for i=1:nsteps
     if i == 0
         initialise(size, nf);
