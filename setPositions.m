6743de207fa2089a97ec448edc36c5cc4a260cbd
MadboyJames <Jkc36@pitt.edu>
2019-03-13 15:20:06 +0000

    Duplicate file out of folder

--- setPositions.m
+++
@@ -1,7 +0,0 @@
-function [agents] = setPositions(arrfish)
-%SETPOSITIONS Summary of this function goes here
-%   Detailed explanation goes here
-for i=1:length(arrfish)
-    agent{i}.position = agent{i}.move();
-end
-
